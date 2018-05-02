157846245cdb55967d6023a267a9d63c542d6aab
Muhcoj <jochum.de.bueger@gmail.com>
2018-05-02 19:18:55 +0200

    Created cvs uploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_26_130605) do
+ActiveRecord::Schema.define(version: 2018_05_02_154454) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "manager"
+    t.string "status"
+    t.integer "terms"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "invoices", force: :cascade do |t|
     t.datetime "date"
